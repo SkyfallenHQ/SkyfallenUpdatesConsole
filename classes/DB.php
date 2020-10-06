f173c13e74f79fe4edf5eae70ff27355867d29d1
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-10-06 14:47:47 +0300

    Add composer package

---
+++ classes/DB.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace SkyfallenUpdatesConsole;
+
+
+class DB
+{
+
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0d334bdf50e19890f94532b5f8f70793548059d3',
+    'name' => 'skyfallen/skyfallenupdateconsole',
+  ),
+  'versions' => 
+  array (
+    'czproject/git-php' => 
+    array (
+      'pretty_version' => 'v3.18.1',
+      'version' => '3.18.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'db664a8d5a1fbfd33e1cdd6ae65bc411bd18dc73',
+    ),
+    'skyfallen/skyfallenupdateconsole' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0d334bdf50e19890f94532b5f8f70793548059d3',
+    ),
+  ),
+);
