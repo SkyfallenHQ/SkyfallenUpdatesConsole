f95c4459ff8fd3f92b88e968c4dfc2d1647d309c
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-10-06 10:32:36 +0300

    Start development

---
+++ SkyfallenCodeLib/AppAPIs/AppCenterAPI.php
@@ -0,0 +1,94 @@
+<?php
+
+
+namespace SkyfallenCodeLibrary;
+
+
+class AppCenterAPI
+{
+    public static function listpublicapps($username,$endpoint = API_ENDPOINT){
+        //The url you wish to send the POST request to
+        $fields = [
+            'username'          => $username,
+            'action'            => "listpublicapps"
+        ];
+
+        $fields_string = http_build_query($fields);
+
+        $ch = curl_init();
+
+        curl_setopt($ch,CURLOPT_URL, $endpoint);
+        curl_setopt($ch,CURLOPT_POST, true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec($ch);
+        $http_resp = curl_getinfo($ch)["http_code"];
+        if($http_resp == 200) {
+            $resp_array = json_decode($result);
+            return $resp_array;
+        } elseif($http_resp == 403){
+            return false;
+        } else {
+            die("Skyfallen App Center API has encountered an unknown error.");
+        }
+    }
+    public static function listprivateapps($username,$password,$endpoint = API_ENDPOINT){
+        //The url you wish to send the POST request to
+        $fields = [
+            'username'          => $username,
+            'password'          => $password,
+            'action'            => "listprivateapps"
+        ];
+
+        $fields_string = http_build_query($fields);
+
+        $ch = curl_init();
+
+        curl_setopt($ch,CURLOPT_URL, $endpoint);
+        curl_setopt($ch,CURLOPT_POST, true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec($ch);
+        $http_resp = curl_getinfo($ch)["http_code"];
+        if($http_resp == 200) {
+            $resp_array = json_decode($result);
+            return $resp_array;
+        } elseif($http_resp == 403){
+            return false;
+        } else {
+            die("Skyfallen App Center API has encountered an unknown error.");
+        }
+    }
+    public static function listpublicapps($username,$endpoint = API_ENDPOINT){
+        //The url you wish to send the POST request to
+        $fields = [
+            'username'          => $username,
+            'action'            => "listpublicapps"
+        ];
+
+        $fields_string = http_build_query($fields);
+
+        $ch = curl_init();
+
+        curl_setopt($ch,CURLOPT_URL, $endpoint);
+        curl_setopt($ch,CURLOPT_POST, true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec($ch);
+        $http_resp = curl_getinfo($ch)["http_code"];
+        if($http_resp == 200) {
+            $resp_array = json_decode($result);
+            return $resp_array;
+        } elseif($http_resp == 403){
+            return false;
+        } else {
+            die("Skyfallen App Center API has encountered an unknown error.");
+        }
+    }
+}
---
+++ configuration.php
@@ -0,0 +1,41 @@
+<?php
+require_once "classes/error.php";
+
+//Allow Registration.
+define("ALLOW_REGISTER",true);
+
+// Update Server Database
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'main');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'updatesconsole');
+// Finish editing
+global $link;
+
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if ($link === false) {
+    Errors::killapp("Database connection could not be successfully established. Application will now stop all processes.", "App Crashed");
+}
+
+// Login Server Database
+define('DB_SERVER_LOGIN', 'localhost');
+define('DB_USERNAME_LOGIN', 'main');
+define('DB_PASSWORD_LOGIN', '');
+define('DB_NAME_LOGIN', 'appcenter');
+// Finish editing
+global $loginlinkink;
+
+$loginlink = mysqli_connect(DB_SERVER_LOGIN, DB_USERNAME_LOGIN, DB_PASSWORD_LOGIN, DB_NAME_LOGIN);
+
+if ($loginlink === false) {
+    Errors::killapp("Database connection to developer login server could not be successfully established. Application will now stop all processes.", "App Crashed");
+}
+// Code Required By Skyfallen Code Library Classes
+// Do not edit
+define("API_ENDPOINT","https://devlogin.api.theskyfallen.com");
+define("API_TYPE","SKYFALLENAPPCENTERAPI");
+define("CONNECTION_INFASTRUCTURE","INTERNET");
+define("SANDOX",false);
+define("APPNAME","SkyfallenUpdatesConsole");
+define("APPID","459d38c4d473e7dfdd464e01b9955eb9");
