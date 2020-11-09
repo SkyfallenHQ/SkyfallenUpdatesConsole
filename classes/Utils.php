c5d918e4e0e3897dfe96a0a05a28a13996ce691c
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-11-09 21:53:57 +0300

    Update Utils.php

--- classes/Utils.php
+++ classes/Utils.php
@@ -56,7 +56,7 @@ class Utils
             foreach ($objects as $object) {
                 if ($object != "." && $object != "..") {
                     if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object))
-                        removedir($dir. DIRECTORY_SEPARATOR .$object);
+                        Utils::removedir($dir. DIRECTORY_SEPARATOR .$object);
                     else
                         unlink($dir. DIRECTORY_SEPARATOR .$object);
                 }
